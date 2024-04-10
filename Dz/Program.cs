d3589012ee4e087a80589f28776c12423878a869
Yarik Mushyts <swat.www.ru.56@mail.ru>
2024-04-10 17:42:17 +0300

    add user input and variables,block diagram

--- Dz/Program.cs
+++ Dz/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите массив строк через запятую :");
+string stroka = Console.ReadLine()!;
+string [] mass = stroka.Split(',');
+var newmass = new string [mass.Length];
+int index = 0;
