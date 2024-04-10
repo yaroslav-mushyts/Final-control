1bc644169a9e4205e6d4bb38dd86c6f101f08d03
Yarik Mushyts <swat.www.ru.56@mail.ru>
2024-04-10 17:47:03 +0300

    add cyle and body

--- Dz/Program.cs
+++ Dz/Program.cs
@@ -1,5 +1,17 @@
 ﻿Console.WriteLine("Введите массив строк через запятую :");
+Console.WriteLine();
 string stroka = Console.ReadLine()!;
 string [] mass = stroka.Split(',');
 var newmass = new string [mass.Length];
 int index = 0;
+
+for (int i = 0; i < mass.Length; i++)
+{
+    var mass2 = mass[i];
+    if (mass2.Length <=3)
+    {
+       newmass[index] = mass2;
+       index++;        
+    }
+
+}
